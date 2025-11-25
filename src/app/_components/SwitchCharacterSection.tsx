0330f255c00fb47b7af307bc37050f6308acd7fb
ranaufalmuha <ifal.muha@gmail.com>
2025-11-25 20:10:53 +0700

    feat: add

--- src/app/_components/SwitchCharacterSection.tsx
+++ src/app/_components/SwitchCharacterSection.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function SwitchCharacterSection() {
   return (
-    <section className="mx-auto max-w-7xl w-full px-4">
+    <section className="mx-auto max-w-7xl flex flex-col w-full px-4 gap-4">
       <div className="w-full bg-accent-1 aspect-video"></div>
+
+      {/* list character  */}
+      <div className="flex gap-4">
+        <div className="bg-accent-1 w-full aspect-2/1 hover:"></div>
+        <div className="bg-accent-1 w-full aspect-2/1"></div>
+        <div className="bg-accent-1 w-full aspect-2/1"></div>
+      </div>
     </section>
   );
 }
